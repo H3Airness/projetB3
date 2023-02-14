6626edd915d4ead48c10629b4195086b644df841
Fantik94 <84931090+Fantik94@users.noreply.github.com>
2023-02-14 20:58:18 +0100

    page NotFound

--- code/airneis/src/composants/front/NotFound.jsx
+++ code/airneis/src/composants/front/NotFound.jsx
@@ -2,10 +2,17 @@ import { NavLink } from "react-router-dom";
 
 const NotFound = () => {
     return ( <>
-    <div className="bg-warning rounded">
-        <h1>Erreur 404</h1>
-        <a className="text-primary text-decoration-none fs-4"><NavLink to="/" className="nav-link">Acceuil</NavLink></a>
-    </div>
+   <h1 className="p-3 display-2 text-center rounded">Erreur 404 
+   <br>
+   </br>
+   <small>Page introuvable</small></h1>
+         <div className="text-center">
+            <img src="giphy.gif" alt="" className="mb-2"/>
+         </div>
+         <div className="bg-dark p-1 mt-5 rounded">
+        <a className= "text-white text-center text-decoration-none fs-5"><NavLink to="/" className="nav-link">Retourner sur le catalogue</NavLink></a>
+        </div>
+   
     </> 
     );
 }
