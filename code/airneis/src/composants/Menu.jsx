6299dcb5aac40029efaa7d3dc63841c4559b47e6
Fantik94 <84931090+Fantik94@users.noreply.github.com>
2023-02-28 22:19:28 +0100

    navbar-logo

--- code/airneis/src/composants/Menu.jsx
+++ code/airneis/src/composants/Menu.jsx
@@ -2,13 +2,13 @@ import { NavLink } from "react-router-dom"
 
 function Menu() {
     return ( 
-        <div class="test" className="bg-dark mb-3" >
-            <nav className="navbar navbar-expand navbar-dark">
+        <div className="bg-dark mb-3">
+            <nav className="navbar navbar-expand navbar-dark nav-bg">
                 <ul className="navbar-nav NavItems">
                     <li className="nav-item">
                         <NavLink to="/" className={({isActive}) => {
                             return isActive ? "nav-link active text-light" : "nav-link"
-                        }}><span className="navbar-brand fs-3 NavTitre">Àirneis</span></NavLink>
+                        }}><span className="NavTitre">Àirneis</span></NavLink>
                     </li>
                     <li className="nav-item">
                         <NavLink to="/connexion" className={({isActive}) => {
@@ -32,10 +32,7 @@ function Menu() {
                         <NavLink to="/" className={({isActive}) => {
                             return isActive ? "nav-link active text-light" : "nav-link"
                         }}><img class="icone-recherche" src="./list_FILL0_wght400_GRAD0_opsz48.png"></img> </NavLink>
-                    </li>
-
-
-                    
+                    </li>                    
                 </ul>
             </nav>
         </div>
