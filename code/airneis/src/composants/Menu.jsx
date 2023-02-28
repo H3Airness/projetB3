d8020aaeb59a1a97c6c8caa33ba3ee385e17f922
Fantik94 <ringlerbaptiste@gmail.com>
2023-02-28 20:22:15 +0100

    header-icon

--- code/airneis/src/composants/Menu.jsx
+++ code/airneis/src/composants/Menu.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom"
 
 function Menu() {
     return ( 
-        <div className="bg-dark mb-3">
+        <div class="test" className="bg-dark mb-3" >
             <nav className="navbar navbar-expand navbar-dark">
                 <ul className="navbar-nav NavItems">
                     <li className="nav-item">
@@ -11,20 +11,31 @@ function Menu() {
                         }}><span className="navbar-brand fs-3 NavTitre">Àirneis</span></NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink to="/panier" className={({isActive}) => {
-                            return isActive ? "nav-link active text-light" : "nav-link"
-                        }}>Panier</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/recherche" className={({isActive}) => {
-                            return isActive ? "nav-link active text-light" : "nav-link"
-                        }}>Recherche</NavLink>
-                    </li>
-                    <li className="nav-item">
                         <NavLink to="/connexion" className={({isActive}) => {
                             return isActive ? "nav-link active text-light" : "nav-link"
                         }}>Connexion</NavLink>
                     </li>
+                </ul>
+                <ul className="navbar-nav NavItems ms-auto">
+                    <li className="nav-item">
+                        <NavLink to="/" className={({isActive}) => {
+                            return isActive ? "nav-link active text-light" : "nav-link"
+                        }}><img class="icone-recherche" src="./search_FILL0_wght400_GRAD0_opsz48.png"></img> </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/Panier" className={({isActive}) => {
+                            return isActive ? "nav-link active text-light" : "nav-link"
+                        }}><img class="icone-recherche" src="./shopping_cart_FILL0_wght400_GRAD0_opsz48.png"></img> </NavLink>
+                    </li>
+
+                    <li className="nav-item">
+                        <NavLink to="/" className={({isActive}) => {
+                            return isActive ? "nav-link active text-light" : "nav-link"
+                        }}><img class="icone-recherche" src="./list_FILL0_wght400_GRAD0_opsz48.png"></img> </NavLink>
+                    </li>
+
+
+                    
                 </ul>
             </nav>
         </div>
