0a51ede61f0e0f5b86cc7683d3f151b3110bc327
Bagass0 <80489889+Bagass0@users.noreply.github.com>
2023-03-01 13:53:15 +0100

    merge commit

--- code/airneis/src/App.jsx
+++ code/airneis/src/App.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Footer from './composants/Footer'
 
 function App() {
 
   return (<>
     <div className="page">
       <Outlet />
+    </div>
+    <div class="sticky-bottom">
+        <Footer/>
     </div>
     </>
   )
