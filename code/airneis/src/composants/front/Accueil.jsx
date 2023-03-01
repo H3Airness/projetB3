06d27dc25249e74e7aa82e1272fb50f802047465
Bagass0 <80489889+Bagass0@users.noreply.github.com>
2023-03-01 16:47:46 +0100

    Correction Airneis

--- code/airneis/src/composants/front/Accueil.jsx
+++ code/airneis/src/composants/front/Accueil.jsx
@@ -19,17 +19,17 @@ const Accueil = () => {
 
     <div class="cat">
     <div className="row row1">
-    <div className="col mb-5 mt-5">
+    <div className="col mb-5 mt-5 DivImages">
       <NavLink to={"/"}>
         <img width={400} src={"https://picsum.photos/800/600?random=1"}/> 
       </NavLink>
     </div>
-    <div className="col mb-5 mt-5">
+    <div className="col mb-5 mt-5 DivImages">
       <NavLink to={"/"}>
         <img width={400} src={"https://picsum.photos/800/600?random=2"} />
       </NavLink>
     </div>
-    <div className="col mb-5 mt-5">
+    <div className="col mb-5 mt-5 DivImages">
       <NavLink to={"/"}>
         <img width={400} src={"https://picsum.photos/800/600?random=3"} />
       </NavLink>
