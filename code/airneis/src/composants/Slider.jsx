b9d3021a9e96858544d485d2c20f5589cfc28a43
H3Airness <damienraunier@gmail.com>
2023-03-02 09:15:40 +0100

    img carousel v1.0

--- code/airneis/src/composants/Slider.jsx
+++ code/airneis/src/composants/Slider.jsx
@@ -4,9 +4,13 @@ import Carousel from 'better-react-carousel';
 const Gallery = () => {
   const [autoplay, setAutoplay] = useState(true);
   const images = [
-    { src: 'https://t4.ftcdn.net/jpg/02/86/03/97/240_F_286039798_bSv7AUnD51WfrgG5w0rSabV3ujNvLgTn.jpg', alt: 'image 1' },
-    { src: 'https://t4.ftcdn.net/jpg/05/42/62/23/240_F_542622306_WJVksyeDFbSNe1xYr4EF8VMn4vgvUAq9.jpg', alt: 'image 2' },
-    { src: 'https://t4.ftcdn.net/jpg/04/52/66/91/240_F_452669128_ro9I4dD0DxEKuCubfMJgWRQRpS2csaSP.jpg', alt: 'image 3' },
+    { src: '../public/img1.jpeg', alt: 'image 1' },
+    { src: '../public/img2.jpeg', alt: 'image 2' },
+    { src: '../public/img3.jpeg', alt: 'image 3' },
+    { src: '../public/img4.jpeg', alt: 'image 4' },
+    { src: '../public/img5.jpeg', alt: 'image 5' },
+    { src: '../public/img6.jpeg', alt: 'image 6' },
+    { src: '../public/img7.jpeg', alt: 'image 7' },
   ];
 
   const handleInteraction = () => {
