37ca057b5eb825b785e42bb848b50d68c28d3990
Bagass0 <80489889+Bagass0@users.noreply.github.com>
2023-03-03 22:56:45 +0100

    fixed-errors-V1.1

--- code/airneis/src/composants/front/Connexion.jsx
+++ code/airneis/src/composants/front/Connexion.jsx
@@ -10,8 +10,9 @@ function Connexion() {
 
     let formType = {};
     formData.forEach((value, key) => formType[key] = formData.get(key));
-
-    axios.post('http://localhost:8000/enquiry.php', formType, {
+    console.log(formType);
+    
+    axios.post('http://localhost:8001/enquiry.php', formType, {
        headers: {
         'Content-Type' : 'application/json',
        }
