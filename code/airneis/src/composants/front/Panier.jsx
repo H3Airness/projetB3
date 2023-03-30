9c826e6a482cfa5329a74510b8a8e77e934fe3e7
Hugo <hugo.b_pereira@outlook.com>
2023-03-30 16:45:15 +0200

    PanierV1.1.1

--- code/airneis/src/composants/front/Panier.jsx
+++ code/airneis/src/composants/front/Panier.jsx
@@ -7,9 +7,16 @@ const Panier = () => {
     <div className="Min-heightConteinerPanier">    
         <div className="shadow p-3 mb-5 bg-body rounded divArticles">
             <h3 className="text-center">Vos articles</h3>
+            <img className="rounded mx-auto d-block" width={200} src={"https://picsum.photos/800/600?random=3"} />
+            <img className="rounded mx-auto d-block" width={200} src={"https://picsum.photos/800/600?random=3"} />
+            <img className="rounded mx-auto d-block" width={200} src={"https://picsum.photos/800/600?random=3"} />
         </div>
         <div className="shadow p-3 mb-5 bg-body rounded divPrixArticles">
             <h3 className="text-center">Total à payer</h3>
+            <br/>
+            <p>Produits: 100euros</p>
+            <p>Livraison: 5euros</p>
+            <h6>Total: 105euros</h6>
         </div>
     </div>    
     </> );
