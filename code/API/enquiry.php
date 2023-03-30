2fbd1421f608025743a7350494939024ecd80f2a
Hugo <hugo.b_pereira@outlook.com>
2023-03-30 10:15:54 +0200

    Update enquiry.php

--- code/API/enquiry.php
+++ code/API/enquiry.php
@@ -1,7 +1,11 @@
 <?php 
 
 	header('Access-Control-Allow-Origin: *');
+<<<<<<< Updated upstream
 	// header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS');
+=======
+	header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS');
+>>>>>>> Stashed changes
 	header('Access-Control-Allow-Headers: *');
 	header('Content-Type: application/json');
 
@@ -10,8 +14,20 @@
 	$email = $data -> email;
 	$mdp = $data -> password;
 
+<<<<<<< Updated upstream
 	http_response_code(201);
 
 	echo 'Reponse envoye ! '. $email. '||'. $mdp;
 
+=======
+	$user = 'id20162419_counterstrike';
+	$password = 'ProjetB3Dev!@';
+
+	$db = new PDO('mysql:host=airnes.000webhostapp.com;dbname=id20162419_airneis', $user, $password);
+
+	if(isset($db))
+	{
+		echo "CONNECTED";
+	} else echo "NOT CONNECTED";
+>>>>>>> Stashed changes
 ?>
