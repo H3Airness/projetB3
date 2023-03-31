807a265213a9332f5e4dc0e43c8327c4571b41d7
Hugo <hugo.b_pereira@outlook.com>
2023-03-31 15:00:18 +0200

    PanierV1.1.2

--- code/API/enquiry.php
+++ code/API/enquiry.php
@@ -1,18 +1,27 @@
 <?php 
 
-	header('Access-Control-Allow-Origin: *');
+	/*header('Access-Control-Allow-Origin: *');
 	header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS');
 	header('Access-Control-Allow-Headers: *');
-	header('Content-Type: application/json');
+	header('Content-Type: application/json');*/
 
-	$data = json_decode(file_get_contents('php://input'));
+	/*$data = json_decode(file_get_contents('php://input'));
 
 	$email = $data -> email;
 	$mdp = $data -> password;
 
 
-	http_response_code(201);
+	http_response_code(201);*/
 
-	echo 'Reponse envoye ! '. $email. '||'. $mdp;
+	$user = "root";
+	$password = "Admin1234!";
+
+	$db = new PDO('mysql:host=localhost;dbname=airneis', $user, $password);
+
+	if(isset($db))
+	{
+		echo "Connected";
+
+	} else echo "Not Connected";
 
 ?>
