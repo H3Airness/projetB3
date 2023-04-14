5bac92e2d8bed7cf709bbf0d505d9a9493b63040
Fantik94 <84931090+Fantik94@users.noreply.github.com>
2023-04-14 19:00:55 +0200

    menu-fix

--- code/airneis/src/composants/front/MenuNavigation.jsx
+++ code/airneis/src/composants/front/MenuNavigation.jsx
@@ -1,38 +1,57 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
 
 function MenuNavigation() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef(null);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
-  useEffect(() => {
-    const fermerMenu = () => {
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
       setMenuOpen(false);
-    };
+    }
+  };
 
-    window.addEventListener("beforeunload", fermerMenu);
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
-      window.removeEventListener("beforeunload", fermerMenu);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
   return (
     <div className="menu-navigation">
       <div className="icone-menu" onClick={toggleMenu}>
-        <img className="icone-recherche" src="../list_FILL0_wght400_GRAD0_opsz48.png" alt="Menu" />
+        <img
+          className="icone-recherche"
+          src="../list_FILL0_wght400_GRAD0_opsz48.png"
+          alt="Menu"
+        />
       </div>
-      <nav className={`menu ${menuOpen ? "ouvert" : ""}`}>
+      <nav className={`menu ${menuOpen ? "ouvert" : ""}`} ref={menuRef}>
         <ul>
-          <li><NavLink to={"/connexion"}>Se connecter</NavLink></li>
-          <li><NavLink to={"/inscription"}>S'inscrire</NavLink></li>
-          <li><NavLink to={"/cgu"}>CGU</NavLink></li>
-          <li><NavLink to={"/mention-legale"}>Mentions légales</NavLink></li>
-          <li><NavLink to={"/contact"}>Contact</NavLink></li>
-          <li><NavLink to={"/"}>À Propos d'Àirneis</NavLink></li>
+          <li>
+            <NavLink to={"/connexion"}>Se connecter</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/inscription"}>S'inscrire</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/cgu"}>CGU</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/mention-legale"}>Mentions légales</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/contact"}>Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/"}>À Propos d'Àirneis</NavLink>
+          </li>
         </ul>
       </nav>
     </div>
