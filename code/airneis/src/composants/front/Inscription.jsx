1d27358f41350580045d4dd316b883336cc7c596
Fantik94 <84931090+Fantik94@users.noreply.github.com>
2023-04-14 20:15:38 +0200

    fix-header-back

--- code/airneis/src/composants/front/Inscription.jsx
+++ code/airneis/src/composants/front/Inscription.jsx
@@ -1,76 +1,104 @@
-import axios from 'axios';
+import axios from "axios";
 import { NavLink } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import Menu from "../Menu";
 
 function Inscription() {
-  const [response, setResponse] = useState('');
+  const [response, setResponse] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
 
     let formType = {};
-    formData.forEach((value, key) => formType[key] = formData.get(key));
-
+    formData.forEach((value, key) => (formType[key] = formData.get(key)));
 
     async function postData() {
       try {
-        const response = await axios.post('http://airneis.fr:3000/inscription.php', formType, {});
+        const response = await axios.post(
+          "http://airneis.fr:3000/inscription.php",
+          formType,
+          {}
+        );
         setResponse(response.data);
-        if (response.data.status === 'success') {
-          navigate('/connexion');
-        } 
+        if (response.data.status === "success") {
+          navigate("/connexion");
+        }
       } catch (error) {
         console.log(error);
       }
     }
 
     postData();
-  }
+  };
 
   return (
     <>
-    <div className='ConnexionTitre'> 
-    <NavLink to="/" className={({isActive}) => {
-        return isActive ? "nav-link active text-light" : "nav-link"
-      }}> <img className="logo-airneis-connexion" src="logo.svg" alt="" /><span className="titreConnexion">Àirneis</span>
-    </NavLink>
-    </div>
+      <Menu />
 
-  <div className='Min-heightConteiner'>   
-    <div className="login-card">
-      <div className="card-header">
-        <div className="log">Inscription</div>
+      <div className="ConnexionTitre">
+        <NavLink
+          to="/"
+          className={({ isActive }) => {
+            return isActive ? "nav-link active text-light" : "nav-link";
+          }}
+        >
+          {" "}
+          <img className="logo-airneis-connexion" src="logo.svg" alt="" />
+          <span className="titreConnexion">Àirneis</span>
+        </NavLink>
       </div>
-      <form onSubmit={handleSubmit}>
-      {response && <p className='ReponseFormulaire text-center mt-3'>{response.message}</p>}
-        <div className="form-group">
-          <label htmlFor="nom">Nom:</label>
-          <input required="" name="nom" id="nom" type="text" />
+
+      <div className="Min-heightConteiner">
+        <div className="login-card">
+          <div className="card-header">
+            <div className="log">Inscription</div>
+          </div>
+          <form onSubmit={handleSubmit}>
+            {response && (
+              <p className="ReponseFormulaire text-center mt-3">
+                {response.message}
+              </p>
+            )}
+            <div className="form-group">
+              <label htmlFor="nom">Nom:</label>
+              <input required="" name="nom" id="nom" type="text" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input required="" name="email" id="email" type="text" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Mot de passe:</label>
+              <input
+                required=""
+                name="password"
+                id="password"
+                type="password"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="ConfirmPassword">Confirmer mot de passe:</label>
+              <input
+                required=""
+                name="password2"
+                id="password2"
+                type="password"
+              />
+            </div>
+            <div className="form-group">
+              <input value="S'inscrire" type="submit" />
+            </div>
+            <div className="text-center">
+              <NavLink className="compteNav" to="/connexion">
+                Vous avez déjà un compte ?
+              </NavLink>
+            </div>
+          </form>
         </div>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input required="" name="email" id="email" type="text" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Mot de passe:</label>
-          <input required="" name="password" id="password" type="password" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="ConfirmPassword">Confirmer mot de passe:</label>
-          <input required="" name="password2" id="password2" type="password" />
-        </div>
-        <div className="form-group">
-          <input value="s'inscrire" type="submit" />
-        </div>
-        <div className="text-center">
-        <NavLink className="compteNav" to="/connexion">Vous avez déjà un compte ?</NavLink>
-        </div>
-      </form>
-    </div>
-  </div>    
+      </div>
     </>
   );
 }
