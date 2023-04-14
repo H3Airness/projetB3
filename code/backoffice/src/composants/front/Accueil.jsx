97295a3cd7baff463f3d164e9983a5dcdd49e289
Damien <77857194+dada919@users.noreply.github.com>
2023-04-14 10:57:30 +0200

    BackOffice v0.1

---
+++ code/backoffice/src/composants/front/Accueil.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Menu from "../Menu";
+
+
+const Accueil = () => {
+
+    return ( <>
+    
+    <div>
+        <Menu/>
+    </div>
+    <div className="info-airneis mt-5">
+        <div className='DivAcceuil'> 
+            <NavLink to={"http://airneis.fr"} target="_blank" className={({isActive}) => {return isActive ? "nav-link active text-light" : "nav-link"}}> <img className="logo-airneis-connexion" src="logo.svg" alt="" /><span className="texteacceuil">airneis.fr</span></NavLink>
+        </div>
+        <div className='DivAcceuil'> 
+            <NavLink to={"http://airneis.fr:3000"} target="_blank" className={({isActive}) => {return isActive ? "nav-link active text-light" : "nav-link"}}> <img className="logo-airneis-connexion" src="https://www.onda-dias.eu/cms/wp-content/uploads/2018/05/api.png" alt="" /><span className="texteacceuil">API</span></NavLink>
+        </div>
+        <div className='DivAcceuil'> 
+            <NavLink to={"http://86.247.29.14:3000/phpmyadmin/"} target="_blank" className={({isActive}) => {return isActive ? "nav-link active text-light" : "nav-link"}}> <img className="logo-airneis-connexion" src="https://upload.wikimedia.org/wikipedia/commons/9/95/PhpMyAdmin_logo.png" alt="" /><span className="texteacceuil">BDD (phpmyadmin)</span></NavLink>
+        </div>
+
+    </div>
+    </> );
+}
+
+export default Accueil ;
+
