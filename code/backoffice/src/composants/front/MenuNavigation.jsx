dcff0a1e093152aa90e284b9d25607f85eb51a6b
Fantik94 <84931090+Fantik94@users.noreply.github.com>
2023-04-14 22:54:39 +0200

    fix-menu

--- code/backoffice/src/composants/front/MenuNavigation.jsx
+++ code/backoffice/src/composants/front/MenuNavigation.jsx
@@ -3,35 +3,61 @@ import { NavLink } from "react-router-dom";
 
 function MenuNavigation() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef(null);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
-  useEffect(() => {
-    const fermerMenu = () => {
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
       setMenuOpen(false);
-    };
+    }
+  };
 
-    window.addEventListener("beforeunload", fermerMenu);
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
-      window.removeEventListener("beforeunload", fermerMenu);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
-
+  
   return (
     <div className="menu-navigation">
       <div className="icone-menu" onClick={toggleMenu}>
-        <img className="icone-recherche" src="../list_FILL0_wght400_GRAD0_opsz48.png" alt="Menu" />
+        <img
+          className="icone-recherche"
+          src="../list_FILL0_wght400_GRAD0_opsz48.png"
+          alt="Menu"
+        />
       </div>
       <nav className={`menu ${menuOpen ? "ouvert" : ""}`}>
         <ul>
-          <li><NavLink to={"/connexion"}>Se connecter</NavLink></li>
-          <li><NavLink to={"/contact"}>Contact</NavLink></li>
-          <li><NavLink to={"http://airneis.fr"} target="_blank">airneis.fr</NavLink></li>
-          <li><NavLink to={"http://airneis.fr:3000"} target="_blank">API</NavLink></li>
-          <li><NavLink to={"http://86.247.29.14:3000/phpmyadmin/"} target="_blank">BDD</NavLink></li>
+          <li>
+            <NavLink to={"/connexion"}>Se connecter</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/contact"}>Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to={"http://airneis.fr"} target="_blank">
+              airneis.fr
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"http://airneis.fr:3000"} target="_blank">
+              API
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"http://86.247.29.14:3000/phpmyadmin/"}
+              target="_blank"
+            >
+              BDD
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </div>
