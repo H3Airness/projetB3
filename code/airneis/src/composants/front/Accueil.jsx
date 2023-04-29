a360757a8200ad0eea035977c99f0f8ee50a99cd
Damien <damienraunier@gmail.com>
2023-04-29 16:55:35 +0200

    contact v1.0

--- code/airneis/src/composants/front/Accueil.jsx
+++ code/airneis/src/composants/front/Accueil.jsx
@@ -6,34 +6,34 @@ import Menu from "../Menu";
 
 const Accueil = () => {
 
-    return ( <>
+  return ( <>
     
-    <div>
-        <Menu/>
-        <Gallery />
-    </div>
-    <div className="info-airneis mt-5">
-        <p> VENANT DES HAUTES TERRES D'ÉCOSSE</p>
-        <p> NOS MEUBLES SONT IMMORTELS</p>
-    </div>
+  <div>
+    <Menu/>
+    <Gallery />
+  </div>
+  <div className="info-airneis mt-5">
+    <p> VENANT DES HAUTES TERRES D'ÉCOSSE</p>
+    <p> NOS MEUBLES SONT IMMORTELS</p>
+  </div>
 
-    <div className="cat">
+  <div className="cat">
     <div className="content-img">
-    <div className="col mb-5 mt-5 img1 img2">
-      <NavLink to={"/"}>
-        <img width={400} src={"https://picsum.photos/800/600?random=1"}/> 
-      </NavLink>
-    </div>
-    <div className="col mb-5 mt-5 DivImages img1 img2">
-      <NavLink to={"/"}>
-        <img width={400} src={"https://picsum.photos/800/600?random=2"} />
-      </NavLink>
-    </div>
-    <div className="col mb-5 mt-5 DivImages img1 img2">
-      <NavLink to={"/"}>
-        <img width={400} src={"https://picsum.photos/800/600?random=3"} />
-      </NavLink>
-    </div>
+      <div className="col mb-5 mt-5 img1 img2">
+        <NavLink to={"/"}>
+          <img width={400} src={"https://picsum.photos/800/600?random=1"}/> 
+        </NavLink>
+      </div>
+      <div className="col mb-5 mt-5 DivImages img1 img2">
+        <NavLink to={"/"}>
+          <img width={400} src={"https://picsum.photos/800/600?random=2"} />
+        </NavLink>
+      </div>
+      <div className="col mb-5 mt-5 DivImages img1 img2">
+        <NavLink to={"/"}>
+          <img width={400} src={"https://picsum.photos/800/600?random=3"} />
+        </NavLink>
+      </div>
     </div>
   </div>
     </> );
--- code/airneis/src/composants/front/Contact.jsx
+++ code/airneis/src/composants/front/Contact.jsx
@@ -1,10 +1,99 @@
+import axios from "axios";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import Menu from "../Menu";
 
-const MentionLegale = () => {
-    return ( <>
-        <Menu/>
-        <h1>Formulaire de Contact</h1>
-    </> );
+function Contact() {
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    let formType = {};
+    formData.forEach((value, key) => (formType[key] = formData.get(key)));
+
+    async function postData() {
+      try {
+        const response = await axios.post(
+          "http://airneis.fr:3000/contact.php",
+          formType,
+          {}
+        );
+        setResponse(response.data);
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    postData();
+  };
+
+  return (
+    <>
+      <Menu />
+
+      <div className="ConnexionTitre">
+        <NavLink
+          to="/"
+          className={({ isActive }) => {
+            return isActive ? "nav-link active text-light" : "nav-link";
+          }}
+        >
+          {" "}
+          <img className="logo-airneis-connexion" src="logo.svg" alt="" />
+          <span className="titreConnexion">Àirneis</span>
+        </NavLink>
+      </div>
+
+      <div className="cat">
+      <div className="Contact-Info">
+      <div className="contact-log">Information de contact</div>
+        <p className="contact-text">
+        27-33 Av. des Champs-Élysées<br></br>
+        75008,Paris, France.<br></br>
+        airneis@hotmail.com
+        </p>   
+      </div>
+
+
+      <div className="Min-heightConteiner">
+        <div className="contact-card">
+          <div className="card-header">
+            <div className="contact-log">Envoyer nous un message</div>
+          </div>
+          <form onSubmit={handleSubmit}>
+            {response && (
+              <p className="ReponseFormulaire text-center mt-3">
+                {response.message}
+              </p>
+            )}
+            <div className="form-group">
+              <label htmlFor="nom">Nom:</label>
+              <input required="" name="nom" id="nom" type="text" />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input required="" name="email" id="email" type="text" />
+            </div>
+
+            <div className="form-message">
+              <label htmlFor="message">Message:</label>
+              <input required="" name="message" id="message" type="text" />
+            </div>
+            
+            <div className="form-group">
+              <input value="Envoyer" type="submit" />
+            </div>
+
+          </form>
+        </div>
+        </div>
+      </div>
+    </>
+  );
 }
- 
-export default MentionLegale;
+
+export default Contact;
