d747de1897894ba7216e5fa3eddea05fd19b39db
Fantik94 <ringlerbaptiste@gmail.com>
2023-05-02 10:02:59 +0200

    rounded-img

--- code/airneis/src/composants/front/Accueil.jsx
+++ code/airneis/src/composants/front/Accueil.jsx
@@ -21,17 +21,17 @@ const Accueil = () => {
     <div className="content-img">
       <div className="col mb-5 mt-5 img1 img2">
         <NavLink to={"/"}>
-          <img width={400} src={"https://picsum.photos/800/600?random=1"}/> 
+          <img width={400} src={"https://picsum.photos/800/600?random=1"} class='img-thumbnail'/> 
         </NavLink>
       </div>
-      <div className="col mb-5 mt-5 DivImages img1 img2">
+      <div className="col mb-5 mt-5 img1 img2">
         <NavLink to={"/"}>
-          <img width={400} src={"https://picsum.photos/800/600?random=2"} />
+          <img width={400} src={"https://picsum.photos/800/600?random=2"} class='img-thumbnail' />
         </NavLink>
       </div>
-      <div className="col mb-5 mt-5 DivImages img1 img2">
+      <div className="col mb-5 mt-5 img1 img2">
         <NavLink to={"/"}>
-          <img width={400} src={"https://picsum.photos/800/600?random=3"} />
+          <img width={400} src={"https://picsum.photos/800/600?random=3"} class='img-thumbnail' />
         </NavLink>
       </div>
     </div>
