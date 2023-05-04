e461c897cb4e5f5a48e3157d2bcb048daff0fe12
Bagass0 <80489889+Bagass0@users.noreply.github.com>
2023-05-04 10:35:02 +0200

    TestBug

--- code/airneis/src/composants/front/Accueil.jsx
+++ code/airneis/src/composants/front/Accueil.jsx
@@ -1,43 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Gallery from '../Slider';
 import { NavLink } from 'react-router-dom';
 import Menu from "../Menu";
 
-
 const Accueil = () => {
+  const [images, setImages] = useState([]);
 
-    return ( <>
-    
-    <div>
-        <Menu/>
+  useEffect(() => {
+    axios.get('http://airneis.fr:3000/accueil.php')
+      .then(response => setImages(response.data.slice(0, 3)))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <>
+      <div>
+        <Menu />
         <Gallery />
-    </div>
-    <div className="info-airneis mt-5">
+      </div>
+      <div className="info-airneis mt-5">
         <p> VENANT DES HAUTES TERRES D'ÉCOSSE</p>
         <p> NOS MEUBLES SONT IMMORTELS</p>
-    </div>
+      </div>
 
-    <div className="cat">
-    <div className="content-img">
-    <div className="col mb-5 mt-5 img1 img2">
-      <NavLink to={"/"}>
-        <img width={400} src={"https://picsum.photos/800/600?random=1"}/> 
-      </NavLink>
-    </div>
-    <div className="col mb-5 mt-5 DivImages img1 img2">
-      <NavLink to={"/"}>
-        <img width={400} src={"https://picsum.photos/800/600?random=2"} />
-      </NavLink>
-    </div>
-    <div className="col mb-5 mt-5 DivImages img1 img2">
-      <NavLink to={"/"}>
-        <img width={400} src={"https://picsum.photos/800/600?random=3"} />
-      </NavLink>
-    </div>
-    </div>
-  </div>
-    </> );
+      <div className="cat">
+        <div className="content-img">
+          {images.map(image => (
+            <div className="col mb-5 mt-5 img1 img2">
+              <NavLink to={"/"}>
+                <img width={400} src={image.url}/>
+              </NavLink>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Accueil ;
-
+export default Accueil;
