ce6beb6299e1615e7b611ba43a18c5c66922eed0
Bagass0 <80489889+Bagass0@users.noreply.github.com>
2023-05-06 20:42:55 +0200

    MajBackOffice

--- code/backoffice/src/composants/front/Articles.jsx
+++ code/backoffice/src/composants/front/Articles.jsx
@@ -5,6 +5,7 @@ import Menu from "../Menu";
 const Articles = () => {
   const [images, setImages] = useState([]);
   const [selectedImages, setSelectedImages] = useState([]);
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     axios.get('http://airneis.ddns.net:3000/articles.php')
@@ -17,24 +18,31 @@ const Articles = () => {
   const handleImageClick = (index) => {
     const image = images[index];
 
-    if (selectedImages.includes(image)) { // Si l'image est déjà sélectionnée, on la désélectionne
+    if (selectedImages.includes(image)) {
       setSelectedImages(selectedImages.filter(selectedImage => selectedImage !== image));
-    } else if (selectedImages.length < 3) { // Sinon, si on a moins de 3 images sélectionnées, on ajoute l'image à la sélection
+    } else if (selectedImages.length < 3) {
       setSelectedImages([...selectedImages, image]);
-    } else { // Sinon, on affiche un message d'alerte
+    } else {
       alert('Vous ne pouvez pas sélectionner plus de 3 images.');
     }
   };
 
   const handleSubmit = () => {
-    axios.post('http://airneis.ddns.net:3000/update_features.php', { id: selectedImages })
-      .then(response => console.log(response))
+    const data = { images: selectedImages };
+    axios.post('http://airneis.ddns.net:3000/update_features.php', data)
+      .then(response => {
+        // Récupérer le message envoyé par le serveur
+        const message = response.data.message;
+        // Stocker le message dans la variable d'état "message"
+        setMessage(message);
+        // Afficher le message dans la console du navigateur
+        console.log(message);
+      })
       .catch(error => console.log(error));
   }
   
   
   
-
   return (
     <>
       <div>
@@ -62,8 +70,13 @@ const Articles = () => {
         </div>
       </div>
       <div className="d-flex justify-content-center my-3">
-        <button onClick={handleSubmit} className='boutonBackOfficeArticles btn btn-primary'>Appliquer</button>
+        <button onClick={handleSubmit} className='boutonBackOfficeArticles btn btn-danger'>Supprimer des articles</button>
+        <button onClick={handleSubmit} className='boutonBackOfficeArticles btn btn-primary'>Mettre en exposition</button>
+      </div>
+      <div className="d-flex justify-content-center my-3">
+      <button onClick={handleSubmit} className='boutonBackOfficeArticles btn btn-success'>Ajouter des nouveaux articles</button>
       </div> 
+      <p>{message}</p>
     </>
   );
 }
