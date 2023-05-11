a4822d6abad00cf38b266b63b5204cb0f1aa26e7
Fantik94 <84931090+Fantik94@users.noreply.github.com>
2023-05-11 10:43:35 +0200

    fix1.2

--- code/airneis/src/composants/front/Recherche.jsx
+++ code/airneis/src/composants/front/Recherche.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import Menu from "../Menu";
 import { dataContext } from "../context/dataContext"
+import { Link } from "react-router-dom"
 
 const Recherche = () => {
   const [recherche, setRecherche] = useState("");
@@ -83,12 +84,15 @@ const Recherche = () => {
           {resultats.map((resultat) => (
             <div className="col-md-4 mb-3" key={resultat.id}>
               <div className="card">
+              <Link to={`/Produit/${resultat.id}`}>
                 <img
                   className="card-img-top"
                   src={resultat.source}
                   alt={resultat.nom}
                   style={{ objectFit: "cover", height: "300px" }}
                 />
+                </Link>
+
                 <div className="card-body">
                   <h5 className="card-title">{resultat.nom}</h5>
                   <p className="card-text text-primary">{resultat.prix} €</p>
