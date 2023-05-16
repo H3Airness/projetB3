985637febe9ae584ab895f5d8ce6603009e76fdd
Damien <77857194+dada919@users.noreply.github.com>
2023-05-16 18:33:58 +0200

    categorie v1.0

--- code/airneis/src/composants/front/Categorie.jsx
+++ code/airneis/src/composants/front/Categorie.jsx
@@ -4,17 +4,20 @@ import { dataContext } from "../context/dataContext"
 
 function Categorie() {
     const { categorie } = useParams();
-    const [product, setProduct] = useState(null);
+    const [products, setProducts] = useState([]);
     const { ajouter } = useContext(dataContext);
   
     useEffect(() => {
       fetch(`http://airneis.ddns.net:3000/categorie.php?categorie=${categorie}`)
         .then(response => response.json())
-        .then(data => setProduct(data))
+        .then(data => {
+          console.log(data);
+          setProducts(data);
+        })
         .catch(error => console.error(error));
     }, [categorie]);
   
-    if (!product) {
+    if (products.length === 0) {
       return <p>Chargement...</p>;
     }
   
@@ -22,24 +25,25 @@ function Categorie() {
       <>
         <div className="container mt-4">
           <div className="row justify-content-center">
-            {product.map((produit) => (
-              <div className="col-md-4 mb-3" key={produit.id}>
+            {products.map((product) => (
+              <div className="col-md-4 mb-3" key={product.id}>
                 <div className="card">
-                  <Link to={`/Produit/${produit.categorie}`}>
+                  <Link to={`/Produit/${product.categorie}`}>
                     <img
                       className="card-img-top"
-                      src={produit.source}
-                      alt={produit.nom}
+                      src={product.source}
+                      alt={product.titre}
                       style={{ objectFit: "cover", height: "300px" }}
                     />
                   </Link>
   
                   <div className="card-body">
-                    <h5 className="card-title">{produit.nom}</h5>
-                    <p className="card-text text-primary">{produit.prix} €</p>
+                    <h5 className="card-title">{product.nom}</h5>
+                    <p className="card-text">{product.description}</p>
+                    <p className="card-text">{product.prix}€</p>
                     <button
                       className="btn btn-primary"
-                      onClick={() => ajouter(produit)}
+                      onClick={() => ajouter(product)}
                     >
                       Ajouter au panier
                     </button>
@@ -53,4 +57,4 @@ function Categorie() {
     );
   }
   
-  export default Categorie;  
+  export default Categorie;
