bbb175bd199162b65d92ca78b2a6a21711136c93
Damien <77857194+dada919@users.noreply.github.com>
2023-05-19 01:30:18 +0200

    fix affichage catégorie produit

--- code/airneis/src/composants/front/Produit.jsx
+++ code/airneis/src/composants/front/Produit.jsx
@@ -1,18 +1,32 @@
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import React, { useState, useEffect, useContext } from 'react';
 import { dataContext } from "../context/dataContext"
 
 function Produit() {
   const { id } = useParams();
+  const [categories, setCategories] = useState([]);
   const [produit, setProduct] = useState(null);
   const {ajouter} = useContext(dataContext)
 
   useEffect(() => {
     fetch(`http://airneis.ddns.net:3000/produit.php?id=${id}`)
       .then(response => response.json())
-      .then(data => setProduct(data))
+      .then(data => {
+        console.log(data.categorie);
+        setProduct(data);
+        if (data && data.categorie) {
+          axios.get(`http://airneis.ddns.net:3000/categorie/affichage_categorie.php?categorie=${data.categorie}`)
+          .then(response => {
+            console.log(response.data);
+            setCategories(response.data);
+          })
+            .catch(error => console.log(error));
+        }
+      })
       .catch(error => console.error(error));
   }, [id]);
+  
 
   if (!produit) {
     return <p>Chargement...</p>;
@@ -20,16 +34,28 @@ function Produit() {
 
   return (
     <>
-    <img src={`http://airneis.ddns.net:3000/img/${produit.categorie}/banniere.jpg`} alt={produit.categorie} style={{ width: '100%' }}/>
-      <center>
-        <h1>{produit.nom}</h1>
-        <p>{produit.description}</p>
-        <img src={`http://airneis.ddns.net:3000/img/${produit.source}`} alt={produit.titre} />
-        <p>{produit.prix}€</p>
-        <button className="btn btn-primary" onClick={() => ajouter(produit) }>
-          <span>Ajouter au panier</span>
-        </button>
-      </center>
+      {categories.length > 0 && categories[0] && (
+        <>
+          <img
+            src={`http://airneis.ddns.net:3000/img/${categories[0].nom}/banniere.jpg`}
+            alt={categories[0].nom}
+            style={{ width: '100%' }}
+          />
+          <div className="ContactTitre">
+            <p>{categories[0].nom}</p>
+          </div>
+
+          <center>
+            <h1>{produit.nom}</h1>
+            <p>{produit.description}</p>
+            <img src={`http://airneis.ddns.net:3000/img/${produit.source}`} alt={produit.titre} />
+            <p>{produit.prix}€</p>
+            <button className="btn btn-primary" onClick={() => ajouter(produit) }>
+              <span>Ajouter au panier</span>
+            </button>
+          </center>
+        </>
+      )}
     </>
   );
 }
