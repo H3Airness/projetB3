3c0e77798fc37ce7dc3cd37f2f81dc3a2cf4f541
Fantik94 <84931090+Fantik94@users.noreply.github.com>
2023-05-23 01:19:09 +0200

    bouton-mdp

--- code/airneis/src/composants/front/MesParametres.jsx
+++ code/airneis/src/composants/front/MesParametres.jsx
@@ -4,9 +4,11 @@ import Connexion from "./Connexion";
 import axios from 'axios';
 
 function MesParametres() {
-  const { isLoggedIn, accountId } = useContext(AuthContext);
+  const { isLoggedIn, accountId, logout } = useContext(AuthContext);
   const [accountInfo, setAccountInfo] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [newPassword, setNewPassword] = useState('');
 
   useEffect(() => {
     const storedAccountInfo = localStorage.getItem('accountInfo');
@@ -23,7 +25,7 @@ function MesParametres() {
       if (isLoggedIn) {
         const response = await axios.post('http://airneis.ddns.net:3000/compte.php', {
           accountId,
-          isLoggedIn: isLoggedIn, // Utilisation de la variable isLoggedIn
+          isLoggedIn: isLoggedIn,
         });
         if (response.data.status === "success") {
           setAccountInfo(response.data.accountInfo);
@@ -40,26 +42,60 @@ function MesParametres() {
 
   const handleLogout = () => {
     localStorage.removeItem('accountInfo');
-    // Ajoutez ici votre code de déconnexion ou de nettoyage supplémentaire
+    logout(); // Appel de la fonction de déconnexion fournie par le contexte
+  };
+
+  const handleEditPassword = () => {
+    setIsEditMode(true);
+  };
+
+  const handleSubmitPassword = (e) => {
+    e.preventDefault();
+    // Logique de soumission du nouveau mot de passe
+    setIsEditMode(false); // Fin de l'édition
+  };
+
+  const handleChangePassword = (e) => {
+    setNewPassword(e.target.value);
   };
 
   if (loading) {
     return <div>Chargement...</div>;
   }
 
+  const maskPassword = (password) => {
+    return isEditMode ? password : '•'.repeat(Math.min(password.length, 6));
+  };
+
   return (
     <>
       {isLoggedIn ? (
-        <>
-          <h1 className="text-center">Récapitulatif de votre compte</h1>
-          <p>Nom: {accountInfo.nom}</p>
-          <p>E-mail: {accountInfo.email}</p>
-          <button onClick={handleLogout}>Se déconnecter</button>
-        </>
+        <div className="mon-compte-container">
+          <div className="sidebar">
+            <h1 className="sidebar-title">Récapitulatif de votre compte</h1>
+            <p>Nom: {accountInfo.nom}</p>
+            <p>E-mail: {accountInfo.email}</p>
+            <div className="password-container">
+              <p className="password">Mot de passe: {maskPassword(accountInfo.password)}</p>
+              {!isEditMode && (
+                <button className="bouton-edition" onClick={handleEditPassword}>
+                  Modifier le mot de passe
+                </button>
+              )}
+            </div>
+            {isEditMode && (
+              <form onSubmit={handleSubmitPassword}>
+                <input type="password" value={newPassword} onChange={handleChangePassword} />
+                <button type="submit">Valider</button>
+              </form>
+            )}
+            <button className="bouton-deconnexion" onClick={handleLogout}>
+              Déconnexion
+            </button>
+          </div>
+        </div>
       ) : (
-        <>
-          <Connexion/>
-        </>
+        <Connexion />
       )}
     </>
   );
