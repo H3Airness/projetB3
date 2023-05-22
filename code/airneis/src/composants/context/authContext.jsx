dbbd68140b6347d97e349ade81f6565b6a27a808
Damien <77857194+dada919@users.noreply.github.com>
2023-05-22 19:53:11 +0200

    mode-connecté v1.0

--- code/airneis/src/composants/context/authContext.jsx
+++ code/airneis/src/composants/context/authContext.jsx
@@ -5,27 +5,36 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [accountId, setAccountId] = useState(null); // Nouvelle variable d'état pour l'ID du compte
 
   useEffect(() => {
     const isLoggedInCookie = Cookies.get('isLoggedIn');
 
     if (isLoggedInCookie) {
       setIsLoggedIn(true);
+      // Récupérer l'ID du compte depuis les cookies ou votre source de données
+      const accountIdCookie = Cookies.get('accountId');
+      setAccountId(accountIdCookie);
     }
   }, []);
 
-  const login = () => {
+  const login = (accountId) => {
     setIsLoggedIn(true);
+    setAccountId(accountId);
     Cookies.set('isLoggedIn', true, { expires: 1 / 24 }); // 1h
+    Cookies.set('accountId', accountId, { expires: 1 / 24 }); // Stocker l'ID du compte dans les cookies
+    console.log('ID du compte:', accountId);
   };
 
   const logout = () => {
     setIsLoggedIn(false);
+    setAccountId(null);
     Cookies.remove('isLoggedIn');
+    Cookies.remove('accountId');
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+    <AuthContext.Provider value={{ isLoggedIn, accountId, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
--- code/airneis/src/composants/front/MesParametres.jsx
+++ code/airneis/src/composants/front/MesParametres.jsx
@@ -1,23 +1,48 @@
 import { AuthContext } from "../context/authContext";
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Connexion from "./Connexion";
+import axios from 'axios';
 
 function MesParametres() {
-    const { isLoggedIn } = useContext(AuthContext);
- 
+  const { isLoggedIn, accountId } = useContext(AuthContext);
+  const [accountInfo, setAccountInfo] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchAccountInfo();
+  }, []);
+
+  const fetchAccountInfo = async () => {
+    try {
+      const response = await axios.post('http://airneis.ddns.net:3000/compte.php', { accountId });
+      if (response.data.status === "success") {
+        setAccountInfo(response.data.accountInfo);
+      } else {
+        console.error(response.data.message);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if (loading) {
+    return <div>Chargement...</div>;
+  }
 
   return (
     <>
-    {isLoggedIn ? (
-            <>
-              <h1 className="text-center">Récapitulatif de votre compte</h1>
-
-            </>
-          ) : (
-            <>
-              <Connexion/>
-            </>
-          )}
+      {isLoggedIn ? (
+        <>
+          <h1 className="text-center">Récapitulatif de votre compte</h1>
+          <p>Nom: {accountInfo.nom}</p>
+          <p>E-mail: {accountInfo.email}</p>
+        </>
+      ) : (
+        <>
+          <Connexion/>
+        </>
+      )}
     </>
   );
 }
