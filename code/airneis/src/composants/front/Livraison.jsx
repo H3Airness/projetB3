970ae622085cb36c04fb58849608c9b486bff9ca
Hugo <hugo.b_pereira@outlook.com>
2023-06-07 17:22:26 +0200

    PageLivraisonUpdate

--- code/airneis/src/composants/front/Livraison.jsx
+++ code/airneis/src/composants/front/Livraison.jsx
@@ -1,24 +1,100 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { useState, useContext } from "react";
+import { dataContext } from "../context/dataContext";
 
+const Livraison = () => {
+  const { panier, getTotalPanier, getTotalProduit } = useContext(dataContext);
+  const [adresse, setAdresse] = useState("");
+  const [numeroCarte, setNumeroCarte] = useState("");
 
-function Livraison() {
+  const handleAdresseChange = (e) => {
+    setAdresse(e.target.value);
+  };
+
+  const handleNumeroCarteChange = (e) => {
+    setNumeroCarte(e.target.value);
+  };
+
+  const handlePayer = () => {
+    // Logique de traitement du paiement avec l'adresse et le numéro de carte
+    // ...
+  };
 
   return (
     <>
-        <center>
-            <div className="ContactTitre">Vous allez trop vite</div>
-            <span>Cette page est en développement</span>
-            <br/>
-            <img width={200} src="https://www.badenconsulting.com/wp-content/uploads/ISO_7010_W001.svg_.png" alt="" /> 
-            <br/>
-            <br/>
-            <NavLink to="/Panier">
-                <button className="btn btn-warning">Retourner au panier</button>
-            </NavLink>
-        </center>
+      <h1 className="mb-4 text-center">Paiement</h1>
+      <div className="rounded flex-column Min-heightConteinerPanier">
+        <div className="shadow p-1 mb-4 bg-body rounded divArticles">
+          <h3 className="text-center mb-5">Vos articles sélectionnés</h3>
+          <table className="table">
+            <tbody className="vertical-align">
+              {panier.map((produit) => {
+                return (
+                  <tr key={produit.id}>
+                    <td>
+                      <img
+                        className="rounded d-block"
+                        width={150}
+                        src={`http://airneis.ddns.net:3000/img_produit/${produit.id}`}
+                        alt={'Auccun article selectionné !'}
+                      />
+                    </td>
+
+                    <td>
+                      <span className="mx-2">{produit.quantite}</span>
+                    </td>
+
+                    <td>
+                      {new Intl.NumberFormat("fr-FR", {
+                        style: "currency",
+                        currency: "EUR",
+                      }).format(getTotalProduit(produit))}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        <br />
+        </div>
+        <p>Livraison : 10€</p>
+          <div className="TotalPayer">
+            <h6>
+              Total :{" "}
+              {new Intl.NumberFormat("fr-FR", {
+                style: "currency",
+                currency: "EUR",
+              }).format(getTotalPanier() + 10)}
+            </h6>
+            <div>
+              <input
+                type="text"
+                placeholder="Adresse de livraison"
+                value={adresse}
+                onChange={handleAdresseChange}
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="Numéro de carte bancaire"
+                value={numeroCarte}
+                onChange={handleNumeroCarteChange}
+              />
+            </div>
+            <button className="btn btn-primary" onClick={handlePayer}>
+              Payer
+            </button>
+          </div>
+        <p className="fw-bold">
+          Montant à payer :&nbsp;
+          {new Intl.NumberFormat("fr-FR", {
+          style: "currency",
+          currency: "EUR",
+          }).format(getTotalPanier())}
+        </p>
+      </div>
     </>
   );
-}
+};
 
 export default Livraison;
